7f757723e72e08d6728e34fa81decc0f23779da8
Midwo <michal.dwojak92@gmail.com>
2017-06-23 20:28:20 +0200

    Create MesageModel in Model

---
+++ ExampleMustache MVC/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleMustache_MVC.Models
+{
+    public class MessageModel
+    {
+        public string Nick { get; set; }
+        public string Contents { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
